87b41b6bdd55cd338735b5255623772cb8b04e13
Muhammadaimn <mkasha9669@gmail.com>
2025-12-17 21:05:27 +0500

    kotta-project

---
+++ quiz/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+# class Shablon(models.Model):
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now_add=True)
+
---
+++ quiz/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index, name="index"),
+    path('quiz-page/', quiz_page, name="result"),
+    path('dashboard/', dashboard, name="dashboard"),
+    path('login/', login, name="login"),
+    path('profile/', profile, name="profile"),
+]
