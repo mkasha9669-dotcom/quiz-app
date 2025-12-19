5ff2deccf38449c852c5c9604bac506298b42607
Muhammadaimn <mkasha9669@gmail.com>
2025-12-19 20:46:41 +0500

    last update

--- quiz/urls.py
+++ quiz/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', index, name="index"),
     path('quiz-page/', quiz_page, name="result"),
     path('dashboard/', dashboard, name="dashboard"),
-    path('login/', login, name="login"),
+    path('login/', login_html, name="login"),
+    path('login-enter/', login_page, name="login-enter"),
     path('profile/', profile, name="profile"),
+    path('logout/', logout_page, name="logout"),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -6,13 +7,28 @@ def index(request):
     return render(request, "index.html")
 
 def dashboard(request):
+    if request.user.is_authenticated:
+        return render(request, "dashboard.html")
     return render(request, "dashboard.html")
 
-def login(request):
+def login_html(request):
     return render(request, "login.html")
 
-def profile(request):
-    return render(request, "settings.html")
+def login_page(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        parol = request.POST.get("password")
+        user = authenticate(request, username=email, password=parol)
+        if user is not None:
+            print(user)
+            login(request, user)
+            return redirect("dashboard")
+    return redirect("login")
+
+def logout_page(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect("login")
 
 def profile(request):
     return render(request, "profile.html")
